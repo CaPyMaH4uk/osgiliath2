545f933f07ad1ad59a96cd4eb916bd2b8e5b0759
CaPyMaH4uk <capymah4uk@gmail.com>
2015-06-03 17:32:36 +0400

    Скрипты

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rognar
+ * Date: 03.06.2015
+ * Time: 15:08
+ */
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+class AdminController extends Controller {
+
+    public function getIndex(){
+
+
+            dd([1, 2, 3]);
+
+            print "1";
+    }
+
+}
---
+++ resources/views/admin/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="{{ asset('/assets/admin/css/materialize.min.css') }}"  media="screen,projection"/>
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+</head>
+
+<body>
+
+
+    Here we are
+
+<!--Import jQuery before materialize.js-->
+<script type="text/javascript" src="{{ asset('/assets/admin/js/jquery-1.11.3.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('/assets/admin/js/materialize.min.js') }}"></script>
+</body>
+</html>
